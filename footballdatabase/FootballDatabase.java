637c6024cd4efe6ea1617a4a0dffaa9db86952f6
mustafawan <121519084+mustafawan@users.noreply.github.com>
2024-05-24 13:46:51 +0300

    Add files via upload

---
+++ footballdatabase/FootballDatabase.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package footballdatabase;
+
+public class FootballDatabase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       HomePage Home = new HomePage();
+       LoginPage Login = new LoginPage();
+       Home.setVisible(true);
+    }   
+}
